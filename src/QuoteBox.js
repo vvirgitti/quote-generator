3e22d149a29f52c5422167793de79f6cdeffb83e
Vanessa Virgitti <virgitti@gmail.com>
2018-05-17 13:40:54 +0100

    Http calls to an api and display the result in the box

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -6,20 +6,29 @@ export class Quote extends React.Component {
   constructor () {
     super ()
     this.state = {
-      quote: ''
+      quote: '',
+      author: ''
     }
     this.handleClick = this.handleClick.bind(this)
   }
 
   handleClick () {
-    axios.get('http://quotes.rest/qod.json')
-    .then(response => console.log(response))
+    return axios.get('https://talaikis.com/api/quotes/random')
+    .then(response => {
+      const quoteAndAuthor = {
+        quote: response.data.quote,
+        author: response.data.author
+      }
+      return this.setState({quote: quoteAndAuthor.quote, author: quoteAndAuthor.author})
+    })
   }
 
   render () {
     return (
       <div className="Box">
         <button className="Button" onClick={this.handleClick}>New Quote</button>
+        <h2>"{this.state.quote}"</h2>
+        <h3>{this.state.author}</h3>
       </div> 
     );
   }
